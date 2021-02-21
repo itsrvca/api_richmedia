745fd2954b702c72f00a7c9d418ffeb240f9ed0f
itsrvca <sureitsrv.ca@gmail.com>
2021-02-21 00:23:57 -0500

    first commit

---
+++ class/Rest.php
@@ -0,0 +1,59 @@
+<?php
+class Rest{
+    private $host  = 'localhost';
+    private $user  = 'root';
+    private $password   = "";
+    private $database  = "emp";      
+    private $empTable = 'emp';	
+	private $dbConnect = false;
+
+    public function __construct(){
+        if(!$this->dbConnect){ 
+            $conn = new mysqli($this->host, $this->user, $this->password, $this->database);
+            if($conn->connect_error){
+                die("Error failed to connect to MySQL: " . $conn->connect_error);
+            }else{
+                $this->dbConnect = $conn;
+            }
+        }
+    }
+	private function getData($sqlQuery) {
+		$result = mysqli_query($this->dbConnect, $sqlQuery);
+		if(!$result){
+			die('Error in query: '. mysqli_error());
+		}
+		$data= array();
+		while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) {
+			$data[]=$row;            
+		}
+		return $data;
+	}
+	private function getNumRows($sqlQuery) {
+		$result = mysqli_query($this->dbConnect, $sqlQuery);
+		if(!$result){
+			die('Error in query: '. mysqli_error());
+		}
+		$numRows = mysqli_num_rows($result);
+		return $numRows;
+	}
+	public function getEmployee($empId) {		
+		$sqlQuery = '';
+		if($empId) {
+			$sqlQuery = "WHERE id = '".$empId."'";
+		}	
+		$empQuery = "
+			SELECT id, name, last_name, height, weight, age, batch, description, address, city, province, country, phone, email, website, MAD100, MAD105, MAD110, MAD120, MAD125, MAD200, MAD225, status 
+			FROM ".$this->empTable." $sqlQuery
+			ORDER BY id DESC";	
+		$resultData = mysqli_query($this->dbConnect, $empQuery);
+		$empData = array();
+		while( $empRecord = mysqli_fetch_assoc($resultData) ) {
+			$empData[] = $empRecord;
+		}
+		header('Content-Type: application/json');
+		echo json_encode($empData, JSON_PRETTY_PRINT);
+		//echo json_encode($empData);	
+	}
+
+}
+?>
